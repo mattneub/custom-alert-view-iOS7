0f3ab1f04cc9604d425f634d573a2f411707f79b
Matt Neuburg <matt@tidbits.com>
2014-08-08 07:53:46 -0700

    updated for beta 5

--- CustomAlertViewDemo/CustomAlertViewController.swift
+++ CustomAlertViewDemo/CustomAlertViewController.swift
@@ -4,11 +4,19 @@ import UIKit
 class CustomAlertViewController : UIViewController {
     var transitioner : CAVTransitioner
     
-    init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
+    override init(nibName nibNameOrNil: String!, bundle nibBundleOrNil: NSBundle!) {
         self.transitioner = CAVTransitioner()
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         self.modalPresentationStyle = .Custom
         self.transitioningDelegate = self.transitioner
+    }
+    
+    convenience override init() {
+        self.init(nibName:"CustomAlertViewController", bundle:nil)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("NSCoding not supported")
     }
     
     @IBAction func doDismiss(sender:AnyObject?) {
